c18140e05c076c7c14e018d8feb6d61a53ca7494
mishamsk <mishamsk@gmail.com>
2014-10-05 22:27:07 +0400

    copied templates to start with (content, content-none, 404)

---
+++ content.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The default template for displaying content. Used for index/archive/search.
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="row">
+		<div class="small-12 medium-6 large-8 column">
+			<?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+		</div>
+		<div class="small-12 medium-6 large-4 column">
+			<header>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<?php // FoundationPress_entry_meta(); ?>
+			</header>
+			<div class="entry-content">
+				<?php the_content(__('Continue reading...', 'FoundationPress')); ?>
+			</div>
+		</div>
+	</div> <!-- .row -->
+</article>
