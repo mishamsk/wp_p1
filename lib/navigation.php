555eb4a740b8ef4e5fb7f70e4ee28e0e29aa77a0
mishamsk <mishamsk@gmail.com>
2014-10-07 22:13:28 +0400

    Merged all nag related code in navigation.php

--- lib/navigation.php
+++ lib/navigation.php
@@ -1,5 +1,51 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Customize the output of menus for Foundation top bar
+ */
+
+class top_bar_walker extends Walker_Nav_Menu {
+
+    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
+        $element->has_children = !empty( $children_elements[$element->ID] );
+        $element->classes[] = ( $element->current || $element->current_item_ancestor ) ? 'active' : '';
+        $element->classes[] = ( $element->has_children && $max_depth !== 1 ) ? 'has-dropdown not-click' : '';
+
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    }
+
+    function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+        $item_html = '';
+        parent::start_el( $item_html, $object, $depth, $args );
+
+        $output .= ( $depth == 0 ) ? '<li class="divider"></li>' : '';
+
+        $classes = empty( $object->classes ) ? array() : (array) $object->classes;
+
+        if( in_array('label', $classes) ) {
+            $output .= '<li class="divider"></li>';
+            $item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '<label>$1</label>', $item_html );
+        }
+
+    if ( in_array('divider', $classes) ) {
+        $item_html = preg_replace( '/<a[^>]*>( .* )<\/a>/iU', '', $item_html );
+    }
+
+        $output .= $item_html;
+    }
+
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $output .= "\n<ul class=\"sub-menu dropdown\">\n";
+    }
+
+}
+
 /**
  * Register Menus
  * http://codex.wordpress.org/Function_Reference/register_nav_menus#Examples
@@ -75,64 +121,68 @@ if ( ! function_exists( 'add_menuclass') ) {
  * Display breadcrumbs
  * @visibility - wrapper class (defaults to show-for-medium-up)
 */
-function p1_breadcrumbs($visibility = 'show-for-medium-up') {
-    $links = explode('|',get_the_category_list('|', 'multiple'));
-    $links = implode(preg_replace("(rel=\"category.*\")", "$1 class=\"current\"", $links));
+if ( ! function_exists( 'p1_breadcrumbs') ) :
+	function p1_breadcrumbs($visibility = 'show-for-medium-up') {
+	    $links = explode('|',get_the_category_list('|', 'multiple'));
+	    $links = implode(preg_replace("(rel=\"category.*\")", "$1 class=\"current\"", $links));
 
-    echo '<nav id="breadcrumbs" class="breadcrumbs ' . $visibility . '">';
-	echo $links;
-	echo '</nav><!--// end .breadcrumbs -->';
-}
+	    echo '<nav id="breadcrumbs" class="breadcrumbs ' . $visibility . '">';
+		echo $links;
+		echo '</nav><!--// end .breadcrumbs -->';
+	}
+endif; // p1_breadcrumbs
 
 /**
  * Display pagination
 */
-function p1_pagination() {
-	global $wp_query;
+if ( ! function_exists( 'p1_pagination') ) :
+	function p1_pagination() {
+		global $wp_query;
 
-	$big = 999999999; // This needs to be an unlikely integer
+		$big = 999999999; // This needs to be an unlikely integer
 
-	$paginate_links = paginate_links( array(
-		'base' => str_replace( $big, '%#%', get_pagenum_link($big) ),
-		'current' => max( 1, get_query_var('paged') ),
-		'total' => $wp_query->max_num_pages,
-		'mid_size' => 2,
-		'prev_next' => True,
-	    'prev_text' => __('&laquo;', 'perlovs'),
-	    'next_text' => __('&raquo;', 'perlovs'),
-		'type' => 'array'
-	) );
+		$paginate_links = paginate_links( array(
+			'base' => str_replace( $big, '%#%', get_pagenum_link($big) ),
+			'current' => max( 1, get_query_var('paged') ),
+			'total' => $wp_query->max_num_pages,
+			'mid_size' => 2,
+			'prev_next' => True,
+		    'prev_text' => __('&laquo;', 'perlovs'),
+		    'next_text' => __('&raquo;', 'perlovs'),
+			'type' => 'array'
+		) );
 
-	if ( $paginate_links ) {
-		// if more than 1 page, replace elements and classes, display pagination
+		if ( $paginate_links ) {
+			// if more than 1 page, replace elements and classes, display pagination
 
-		echo '<div id="pagination" class="pagination-centered">';
-		echo '	<ul class="pagination" role="menubar" aria-label="Pagination">';
+			echo '<div id="pagination" class="pagination-centered">';
+			echo '	<ul class="pagination" role="menubar" aria-label="Pagination">';
 
-		if (1 == max( 1, get_query_var('paged') )) // if first page - add disabled left quotes
-			echo '		<li class="arrow unavailable" aria-disabled="true"><a href="#">' . __('&laquo;', 'perlovs') . '</a></li>';
+			if (1 == max( 1, get_query_var('paged') )) // if first page - add disabled left quotes
+				echo '		<li class="arrow unavailable" aria-disabled="true"><a href="#">' . __('&laquo;', 'perlovs') . '</a></li>';
 
-		foreach ($paginate_links as $link) {
-			// replace for current page
-			$link = str_replace( "<span class='page-numbers current'>", '<li class="current"><a href="#">', $link );
-			// replace for dots
-			$link = str_replace( '<span class="page-numbers dots">' . __( '&hellip;' ), '<li class="unavailable" aria-disabled="true"><a href="#">' . __( '&hellip;', 'perlovs' ), $link );
-			$link = str_replace( "</span>", "</a>", $link );
+			foreach ($paginate_links as $link) {
+				// replace for current page
+				$link = str_replace( "<span class='page-numbers current'>", '<li class="current"><a href="#">', $link );
+				// replace for dots
+				$link = str_replace( '<span class="page-numbers dots">' . __( '&hellip;' ), '<li class="unavailable" aria-disabled="true"><a href="#">' . __( '&hellip;', 'perlovs' ), $link );
+				$link = str_replace( "</span>", "</a>", $link );
 
-			// replace for normal numbers
-			$link = str_replace( "<a class='page-numbers'", '<li><a ', $link );
-			// replace for active arrows
-			$link = str_replace( '<a class="prev page-numbers"', '<li class="arrow"><a ', $link );
-			$link = str_replace( '<a class="next page-numbers"', '<li class="arrow"><a ', $link );
+				// replace for normal numbers
+				$link = str_replace( "<a class='page-numbers'", '<li><a ', $link );
+				// replace for active arrows
+				$link = str_replace( '<a class="prev page-numbers"', '<li class="arrow"><a ', $link );
+				$link = str_replace( '<a class="next page-numbers"', '<li class="arrow"><a ', $link );
 
-			echo $link . '</li>';
+				echo $link . '</li>';
+			}
+
+	  		if ($wp_query->max_num_pages == get_query_var('paged')) // if last page - add disabled right quotes
+				echo '		<li class="arrow unavailable" aria-disabled="true"><a href="#">' . __('&raquo;', 'perlovs') . '</a></li>';
+			echo '	</ul>';
+			echo '</div><!--// end .pagination -->';
 		}
-
-  		if ($wp_query->max_num_pages == get_query_var('paged')) // if last page - add disabled right quotes
-			echo '		<li class="arrow unavailable" aria-disabled="true"><a href="#">' . __('&raquo;', 'perlovs') . '</a></li>';
-		echo '	</ul>';
-		echo '</div><!--// end .pagination -->';
 	}
-}
+endif; // p1_pagination
 
 ?>
