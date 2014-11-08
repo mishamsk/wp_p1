fc2a55afcdd080220f706b9ebc95b29ed0bdb5d8
mishamsk <mishamsk@gmail.com>
2014-11-08 11:30:59 +0300

    Working on blog section for front-page

--- front-page.php
+++ front-page.php
@@ -1,18 +1,87 @@
 <?php get_header(); ?>
 	<section id="home-greeting" data-page-name="home">
 		<div class="row">
-			<div class="small-12 small-centered columns">
+			<div class="small-12 columns">
 				<div class="logo"><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 				<p class="text-center"><?php _e( 'We blog about Life, the Universe, and Everything', 'perlovs' ); ?></p>
 			</div> <!-- columns -->
 		</div> <!-- row -->
 	</section> <!-- #home-greeting -->
 	<section id="home-blog" data-page-name="blog">
-		<div class="row">
-			<div class="small-12 small-centered columns">
+		<?php
+		$qblog = new WP_Query(array(
+			    'posts_per_page'   => 3,
+			    'post_type' => 'post'
+			));
 
-			</div> <!-- columns -->
-		</div> <!-- row -->
+		if ( $qblog->have_posts() ) : ?>
+			<div class="row">
+				<div class="small-12 medium-6 columns">
+					<?php $qblog->the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header>
+								<?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+								<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+								<?php // FoundationPress_entry_meta(); ?>
+							</header>
+							<div class="entry-content">
+								<?php the_content(__('Continue reading...', 'prelovs')); ?>
+							</div>
+						</article>
+				</div> <!-- columns -->
+				<div class="small-12 medium-6 columns">
+					<div class="row">
+						<div class="small-12 columns">
+							<?php if ( $qblog->have_posts() ) :
+									$qblog->the_post(); ?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header>
+										<?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+										<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+										<?php // prelovs_entry_meta(); ?>
+									</header>
+									<div class="entry-content">
+										<?php the_content(__('Continue reading...', 'prelovs')); ?>
+									</div>
+								</article>
+							<?php else : ?>
+								<article class="empty">
+									<?php _e( 'That is all for now. We have to write more...', 'perlovs' ); ?>
+								</article>
+							<?php endif; ?>
+						</div> <!-- columns -->
+						<div class="small-12 columns">
+							<?php if ( $qblog->have_posts() ) :
+									$qblog->the_post(); ?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header>
+										<?php if ( has_post_thumbnail() ) the_post_thumbnail(); ?>
+										<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+										<?php // prelovs_entry_meta(); ?>
+									</header>
+									<div class="entry-content">
+										<?php the_content(__('Continue reading...', 'prelovs')); ?>
+									</div>
+								</article>
+							<?php else : ?>
+								<article class="empty">
+									<?php _e( 'That is all for now. We have to write more...', 'perlovs' ); ?>
+								</article>
+							<?php endif; ?>
+						</div> <!-- columns -->
+					</div> <!-- row -->
+				</div> <!-- columns -->
+			</div> <!-- row -->
+		<?php else : ?>
+			<div class="row">
+				<div class="small-8 small-centered columns">
+					<h1 class="text-center"><?php _e( 'Oops, nothing have been posted in here yet...', 'perlovs' ); ?></h1>
+				</div> <!-- columns -->
+			</div> <!-- row -->
+		<?php
+		endif; // if ( $qblog->have_posts() ) :
+
+		wp_reset_postdata();  ?>
 	</section> <!-- #home-blog -->
 	<section id="home-travel" data-page-name="travel">
 		<div class="row">
