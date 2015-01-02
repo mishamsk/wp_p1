9f7f08785fd2e4c2876e058ae723f352d3cd6b49
mishamsk <mishamsk@gmail.com>
2015-01-03 02:45:28 +0300

    Fix wrapping of meta header for article

--- lib/entry-helpers.php
+++ lib/entry-helpers.php
@@ -18,7 +18,7 @@ if(!function_exists('p1_meta')) :
     function p1_meta() {
     	global $post;
 
-    	echo '<div class="post-meta">';
+    	//echo '<div class="post-meta">';
     	// output time
     	$archive_year  = get_the_time('Y');
 		$archive_month = get_the_time('m');
@@ -28,29 +28,77 @@ if(!function_exists('p1_meta')) :
         echo '	<a href="' . get_month_link( $archive_year, $archive_month) . '">' . get_the_time('F') . '</a>, ';
         echo '	<a href="' . get_year_link( $archive_year) . '">' . get_the_time('Y') . '</a>';
         echo '</time>';
+        // divider to allow white-space wrap
+        echo '<span> </span>';
         // output link to post author
         echo '<span class="icon-g-author post-author">';
         echo '	<a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author">'. get_the_author() .'</a></span>';
         // output countries if present
-        if (get_the_terms( $post->ID, 'countries' )) {
+        if (get_the_terms( $post->ID, 'countries' ) && !is_tax('countries') ) {
+            // divider to allow white-space wrap
+            echo '<span> </span>';
         	echo '<span class="icon-g-place post-countries">';
         	echo '	' . get_the_term_list( $post->ID, 'countries', '', ', ' );
         	echo '</span>';
         }
-        echo "</div>";
+        //echo "</div>";
     }
 endif;
 
-/*
-	WP wraps all images in <p> tags which is not nice for css
-*/
+add_filter('the_content', 'filter_ptags_on_images');
 if(!function_exists('filter_ptags_on_images')) :
+    /*
+        WP wraps all images in <p> tags which is not nice for css
+    */
 	function filter_ptags_on_images($content)
 	{
 	    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 	}
 endif; // filter_ptags_on_images
 
-// we want it to be run after the autop stuff... 10 is default.
-add_filter('the_content', 'filter_ptags_on_images');
+add_filter( 'the_title', 'add_default_title', 10, 2 );
+if ( ! function_exists( 'add_default_title' ) ) :
+/**
+ * Add default title to posts without one
+ */
+function add_default_title( $title, $id = null )
+{
+    return $title ? $title : __('No title', 'perlovs');
+}
+endif; // add_default_title
+
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
+if ( ! function_exists( 'remove_more_link_scroll' ) ) :
+/**
+ * Remove hash at more url
+ */
+function remove_more_link_scroll( $link ) {
+    $link = preg_replace( '|#more-[0-9]+|', '', $link );
+    return $link;
+}
+endif; // remove_more_link_scroll
+
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
+if ( ! function_exists( 'modify_read_more_link' ) ) :
+/**
+ * Make pretty more link + add class for styling + add id for analytics
+ */
+function modify_read_more_link() {
+    return '';
+    // return '<a class="more-link" id="more-' .  get_the_ID() . '" href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '">' . __('Continue reading...', 'perlovs') . '</a>';
+}
+endif; // modify_read_more_link
+
+add_filter('excerpt_more', 'modify_excerpt_more');
+if ( ! function_exists( 'modify_excerpt_more' ) ) :
+/**
+ * Make pretty more link + add class for styling + add id for analytics
+ */
+function modify_excerpt_more($more) {
+    return '...';
+    // global $post;
+    // return '...<a class="more-link" id="more-' .  $post->ID . '" href="' . get_permalink($post->ID) . '" title="' . the_title_attribute( array('echo' => '0', 'post' => $post->ID )) . '">' . __('Continue reading...', 'perlovs') . '</a>';
+}
+endif; // modify_excerpt_more
+
 ?>
