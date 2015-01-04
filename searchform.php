2c4cfb93361846ef6b52588636a3aec40f89b81f
mishamsk <mishamsk@gmail.com>
2015-01-04 07:33:49 +0300

    Extract search form to separate stylesheet + make "card" style by default

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form card" action="<?php echo home_url( '/' ); ?>">
 	<p class="search-label"><?php _e( 'Search', 'perlovs' )?></p>
 	<input type="text" class="search-field" placeholder="<?php if(is_front_page()) {
 				_e( 'Looking for something? Just search...', 'perlovs' );
