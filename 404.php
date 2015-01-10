7fe6865fbc7e51ac305339d6a56833e821281125
mishamsk <mishamsk@gmail.com>
2015-01-10 17:23:09 +0300

    Finished 404 page (including new icon)

--- 404.php
+++ 404.php
@@ -1,20 +1,26 @@
-<?php get_header(); ?>
+<?php
+/*
+	Template for 404 page
+*/
 
-<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	<header>
-		<h1 class="entry-title"><?php _e('File Not Found', 'perlovs'); ?></h1>
-	</header>
-	<div class="entry-content">
-		<div class="error">
-			<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'perlovs'); ?></p>
-		</div>
-		<p><?php _e('Please try the following:', 'perlovs'); ?></p>
-		<ul>
-			<li><?php _e('Check your spelling', 'perlovs'); ?></li>
-			<li><?php printf(__('Return to the <a href="%s">home page</a>', 'perlovs'), home_url()); ?></li>
-			<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'perlovs'); ?></li>
-		</ul>
-	</div>
-</article>
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
 
-<?php get_footer(); ?>
+	get_header();
+?>
+	<div class="row">
+		<div class="small-12 columns">
+			<header id="archive-header">
+				<div class="err-404">4<span class="icon-sad"></span>4</div>
+				<h1 class="page-title"><?php _e('File Not Found', 'perlovs'); ?></h1>
+				<h6 class="archive-description"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable. You can try to go <a href="javascript:history.back()">back</a> or use the search below.', 'perlovs'); ?></h6>
+			</header>
+		</div> <!-- .small-12 columns -->
+	</div> <!-- .row-->
+<?php
+	get_template_part( 'blogroll');
+	get_footer();
+?>
