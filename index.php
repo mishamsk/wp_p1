68d12e07bfa9babd2b8224a5f73a1415db2266ac
mishamsk <mishamsk@gmail.com>
2015-01-27 16:09:18 +0300

    Made index same as 404

--- index.php
+++ index.php
@@ -2,6 +2,25 @@
 /**
  * The main template file.
  */
-get_header(); ?>
 
-<?php get_footer(); ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+	get_header();
+?>
+	<div class="row">
+		<div class="small-12 columns">
+			<header id="archive-header">
+				<div class="err-404">4<span class="icon-sad"></span>4</div>
+				<h1 class="page-title"><?php _e('File Not Found', 'perlovs'); ?></h1>
+				<h6 class="archive-description"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable. You can try to go <a href="javascript:history.back()">back</a> or use the search below.', 'perlovs'); ?></h6>
+			</header>
+		</div> <!-- .small-12 columns -->
+	</div> <!-- .row-->
+<?php
+	get_template_part( 'blogroll');
+	get_footer();
+?>
